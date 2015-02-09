9cf6bf6b3ff919be5fc108a8352df30a3c751b5e
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-02-09 19:32:50 +0000

    Auto-generated commit 09/02/2015 7:32:46 PM

--- index.php
+++ index.php
@@ -4,32 +4,15 @@
 <!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
 <!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title('|', true, 'right'); ?></title>
-    <?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php wp_title('|', true, 'right'); ?></title>
+  <?php wp_head(); ?>
 </head>
 <body>
 
-  <nav>
-    <ul class="primary-nav">
-    </ul>
-  </nav>
-  
-  <main class="content">
-    <h1>Coming Soon</h1>
-  </main>
-  
-  <div class="overlay-nav">
-    <span></span>
-  </div>
-  
-  <div class="overlay-content">
-    <span></span>
-  </div>
-  
-  <a href="#0" class="nav-trigger">Menu<span class="icon"></span></a>
-  
-<?php wp_footer(); ?>
+  <h1>Coming Soon</h1>
+
+  <?php wp_footer(); ?>
 </body>
 </html>
